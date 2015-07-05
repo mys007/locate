d56d41615340b7f7b454bcde7d49dfab291097a5
Martin Simonovsky <simonovm@imagine.enpc.fr>
2015-07-06 00:38:59 +0200

    - aligning always to T1, per patient - debug print of image ranges (->can't use PNG for medical data:P)

--- src/tum/slices/extract_slices.cpp
+++ src/tum/slices/extract_slices.cpp
@@ -10,53 +10,84 @@
 #include <itkExtractImageFilter.h>
 #include <itkResampleImageFilter.h>
 #include "itkLinearInterpolateImageFunction.h"
+#include "itkStatisticsImageFilter.h"
 
 #include "boost/filesystem.hpp"
 #include <boost/function.hpp>
+#include <vector>
+#include <algorithm>
 
 typedef double PixelType;
 typedef itk::Image<PixelType,3> Image3DType;
 typedef itk::Image<PixelType,2> Image2DType;
+//typedef itk::Image<unsigned char,2> Image2DType;
 typedef itk::ImageFileReader<Image3DType> ImageFileReaderType;
 typedef itk::ImageFileWriter<Image2DType> ImageFileWriterType;
 typedef itk::ExtractImageFilter<Image3DType,Image2DType> ExtractImageFilterType;
 typedef itk::ResampleImageFilter<Image3DType,Image3DType> ImageResampleFilterType;
 typedef itk::LinearInterpolateImageFunction<Image3DType, double>    InterpolatorType;
 
+
+
+template <typename ImageType> void stddev(const typename ImageType::Pointer image)
+{
+	typedef itk::StatisticsImageFilter<ImageType> StatisticsImageFilterType;
+	typename StatisticsImageFilterType::Pointer statisticsImageFilter = StatisticsImageFilterType::New();
+	statisticsImageFilter->SetInput(image);
+	statisticsImageFilter->Update();
+
+	std::cout << "Mean: " << (double)statisticsImageFilter->GetMean() << std::endl;
+	std::cout << "Std.: " << (double)statisticsImageFilter->GetSigma() << std::endl;
+	std::cout << "Min: " << (double)statisticsImageFilter->GetMinimum() << std::endl;
+	std::cout << "Max: " << (double)statisticsImageFilter->GetMaximum() << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
 	const std::string target_path(argv[1]);
-	boost::filesystem::directory_iterator end_itr;
-	std::string referenceImageName;
+	Image3DType::Pointer referenceImage = NULL;
 	
-	ImageFileReaderType::Pointer  imageReaderReference = ImageFileReaderType::New();
-	referenceImageName = target_path + "IXI002-Guys-0828-T1.nii.gz";
-	imageReaderReference->SetFileName(referenceImageName);
-	Image3DType::Pointer  referenceImage = Image3DType::New();
-	referenceImage = imageReaderReference->GetOutput();
-	referenceImage->Update();
-	referenceImage->DisconnectPipeline();
+    std::vector<boost::filesystem::path> v;
+    std::copy(boost::filesystem::directory_iterator(target_path), boost::filesystem::directory_iterator(), std::back_inserter(v));
+    std::sort(v.begin(), v.end());
 
-	for (boost::filesystem::directory_iterator iDir(target_path); iDir!= end_itr; ++iDir)
-	{
-	if (iDir->path().extension() == ".gz")
+    for (std::vector<boost::filesystem::path>::const_iterator it(v.begin()), it_end(v.end()); it != it_end; ++it)
+    {
+		if (it->extension() == ".gz")
 		{
 			ImageFileReaderType::Pointer  imageReader = ImageFileReaderType::New();
 			ImageFileWriterType::Pointer  imageWriter = ImageFileWriterType::New();
 			ExtractImageFilterType::Pointer  extractSliceFilter = ExtractImageFilterType::New();
 
-			std::string currentFileName(iDir->path().filename().string()) ;
+			std::string currentFileName(it->filename().string());
 			std::cout << "Reading from: "<< argv[1]+currentFileName << std::endl;
 			imageReader->SetFileName(argv[1]+currentFileName);
+
+			if (it->filename().string().find("-T1.nii.gz") != std::string::npos)
+			{
+				std::cout << "Image set as reference" << std::endl;
+				referenceImage = Image3DType::New();
+				referenceImage = imageReader->GetOutput();
+				referenceImage->Update();
+				referenceImage->DisconnectPipeline();
+			}
+
 			InterpolatorType::Pointer interpolator = InterpolatorType::New();
 			ImageResampleFilterType::Pointer imageResampleFilter = ImageResampleFilterType::New();
 			imageResampleFilter->SetInput(imageReader->GetOutput());
-			imageResampleFilter->SetReferenceImage(referenceImage);
+			imageResampleFilter->SetReferenceImage(referenceImage); //align w.r.t. physical coordinates
 			imageResampleFilter->UseReferenceImageOn();
-			imageResampleFilter->SetDefaultPixelValue(0);
+			imageResampleFilter->SetDefaultPixelValue(-1);
 			imageResampleFilter->SetInterpolator(interpolator);
 			imageResampleFilter->Update();
 			//imageReader->Update();
+
+			//Image3DType::PointType origin = imageReader->GetOutput()->GetOrigin();
+			//Image3DType::SpacingType spacing = imageReader->GetOutput()->GetSpacing();
+			//Image3DType::SizeType size_pxl = imageReader->GetOutput()->GetLargestPossibleRegion().GetSize();
+			//std::cout << size_pxl[0] << " " << size_pxl[1] << " " << spacing[0] << " " << spacing[1] << " " << origin[0] << " " << origin[1] << std::endl;
+
+			stddev<Image3DType>(imageReader->GetOutput());
 						
 			for (int iSlice=0; iSlice<3; ++iSlice)
 			{
@@ -81,9 +112,10 @@ int main(int argc, char *argv[])
 				extractSliceFilter->Update();
 
 				std::stringstream outputStream;
-				outputStream << argv[2] <<"Slice_" << iSlice << "_"<<currentFileName ;
+				outputStream << argv[2] <<"Slice_" << iSlice << "_"<<currentFileName;// << ".png";
 				std::cout << "Writing to :  " << outputStream.str() << std::endl;
-				
+
+				stddev<Image2DType>(extractSliceFilter->GetOutput());
 
 				imageWriter->SetFileName(outputStream.str());
 				imageWriter->SetInput(extractSliceFilter->GetOutput());
