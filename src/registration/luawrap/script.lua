90c05c07b7dbdc3e54abc21995329ee2ed606516
Martin Simonovsky <simonovm@imagine.enpc.fr>
2015-07-30 10:48:11 +0200

    added demo how to call lua from c++

---
+++ src/registration/luawrap/script.lua
@@ -0,0 +1,26 @@
+require 'nn'
+require 'cunn'
+package.path = "/home/simonovm/workspace/medipatch/src/myrock/?.lua;" .. package.path
+require('SpatialMaxPoolingCaffe')
+
+local model, inputGpu
+
+function loadNetwork(path)
+	cutorch.setDevice(1)
+	torch.setdefaulttensortype('torch.FloatTensor')
+	torch.manualSeed(1)
+	cutorch.manualSeed(1)
+	
+	model = torch.load(path)
+	model = model:cuda()
+	
+	print('Loaded '..path)
+	print(model)
+end
+
+function forward(input, output)
+	inputGpu = inputGpu or input:cuda()
+	inputGpu:copy(input)
+	xpcall(function() output:copy(model:forward(inputGpu)) end, function(err) print(debug.traceback(err)) end)
+end
+
