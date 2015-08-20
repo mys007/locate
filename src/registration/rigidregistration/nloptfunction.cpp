3d8c81ec90fb2d5561f4a38494eb532848c15c46
gutierrezbecker <ingutbecker@gmail.com>
2015-08-20 18:41:13 +0200

    Added script for rigid  registration

---
+++ src/registration/rigidregistration/nloptfunction.cpp
@@ -0,0 +1,70 @@
+#include "itkEuler3DTransform.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkResampleImageFilter.h"
+
+#include "nloptfunction.h"
+
+typedef itk::Euler3DTransform<double> EulerTransformT;
+typedef itk::ResampleImageFilter<ImageT,ImageT> ResamplerT;
+typedef itk::LinearInterpolateImageFunction<ImageT,double> InterpolatorT;
+
+double nloptSimilarityFunction( const std::vector<double> &transformationVector,
+                                std::vector<double> &grad,
+                                void *dataStructure)
+{
+    NloptDataStruct *m_dataStructure = (NloptDataStruct*)dataStructure;
+    ImageT::Pointer fixedImage = m_dataStructure->m_fixedImage;
+    ImageT::Pointer movingImage = m_dataStructure->m_movingImage;
+    
+    ResamplerT::Pointer resampler = ResamplerT::New();
+    ImageT::SizeType    imgSize = fixedImage->GetLargestPossibleRegion().GetSize();
+    ImageT::Pointer     movedImage = ImageT::New();
+    
+    EulerTransformT::TranslationType translation;
+    std::vector <double> rotation(3,0);
+    
+    rotation[0] = transformationVector[0];
+    rotation[1] = transformationVector[1];
+    rotation[2] = transformationVector[2];
+    translation[0] = transformationVector[3];
+    translation[1] = transformationVector[4];
+    translation[2] = transformationVector[5];
+    
+    EulerTransformT::Pointer eulerTransform = EulerTransformT::New();
+    InterpolatorT::Pointer interpolator = InterpolatorT::New();
+    eulerTransform->SetTranslation(translation);
+    eulerTransform->SetRotation(rotation[0],rotation[1],rotation[2]);
+    ImageT::IndexType centerImageSpace;
+    ImageT::PointType centerWorldSpace;
+    for (int iDim=0; iDim< 3; iDim++)
+       centerImageSpace[iDim] =  imgSize[iDim]/2;
+    fixedImage->TransformIndexToPhysicalPoint(centerImageSpace,centerWorldSpace);
+    
+    eulerTransform->SetCenter(centerWorldSpace);
+    
+    resampler->SetSize(imgSize);
+    resampler->SetInput(movingImage);
+    resampler->SetTransform(eulerTransform);
+    resampler->SetInterpolator(interpolator);
+    resampler->SetReferenceImage(fixedImage);
+    resampler->SetDefaultPixelValue(0);
+    movedImage = resampler->GetOutput();
+    movedImage->Update();
+    
+    m_dataStructure->m_similarityMetric->setFixedImage(fixedImage);
+    m_dataStructure->m_similarityMetric->setMovingImage(movedImage);
+    m_dataStructure->m_similarityMetric->compute();
+    
+    double similarityValue;
+    
+    similarityValue = m_dataStructure->m_similarityMetric->getSimilarityValue();
+    std::cout << transformationVector[0] << " " << transformationVector[1] << " " 
+              << transformationVector[2] << " " << transformationVector[3] << " " 
+              << transformationVector[4] << " " << transformationVector[5] << " " 
+              << ":  " << similarityValue; 
+    
+    std::cout << std::endl;
+        
+    
+    return similarityValue;
+}
---
+++ src/registration/rigidregistration/rigidregistration.cpp
@@ -0,0 +1,214 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkExtractImageFilter.h"
+#include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sstream>
+#include <TH/TH.h>
+
+extern "C"{
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
+#include "luaT.h"
+#include "itkImageRegionConstIterator.h"
+
+#include "NnSimilarityMetric.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "typedefinitions.h"
+
+#include "itkEuler3DTransform.h"
+#include "itkResampleImageFilter.h"
+#include "itkLinearInterpolateImageFunction.h"
+
+#include "tclap/CmdLine.h"
+#include "nlopt.hpp"
+#include "nloptfunction.h"
+
+typedef itk::ImageFileReader<ImageT> ImageFileReaderT;
+typedef itk::ExtractImageFilter<ImageT,ImageT> ExtractFilterT;
+
+int main (int argc, char *argv[])
+{
+    std::cout << "Reading parameters: " <<std::endl;
+    //DATAPATH IS HARD CODED FOR SIMPLICITY
+    //std::string 
+    //dataPath("/home/beckerbe/Multimodal/images/IXI_R/");
+    //////////////////////////////////////////////////////////////
+    TCLAP::CmdLine 
+    cmd("Evaluate the similarity metric on a grid of translations or rotations");
+    
+    TCLAP::ValueArg<std::string> 
+            fixedFileArg ("f", "fixedImage", "Fixed Image", true," ", "FILE"); 
+    cmd.add(fixedFileArg);
+    
+    TCLAP::ValueArg<std::string> 
+            movingFileArg ("m", "movingImage", "Moving Image", true," ", "FILE");
+    cmd.add(movingFileArg);
+    
+    TCLAP::ValueArg<std::string> 
+            networkFileArg ("n", "network", "Network", true," ", "FILE");
+    cmd.add(networkFileArg);
+
+        
+    cmd.parse(argc,argv);
+    // CHANGE HERE THE IMAGES AND THE NETWORKS.
+    const char *netpath =  (networkFileArg.getValue()).c_str();
+    std::string 
+        filenameFixed (fixedFileArg.getValue());
+    std::string
+        filenameMoving(movingFileArg.getValue());
+    
+    
+     // TODO: Can we get the patchsize directly from the network??
+    ImageT::SizeType  patchSize;
+    patchSize[0] = 64;
+    patchSize[1] = 64;
+    patchSize[2] = 1;
+        
+    // THIS IS USED TO DEFINE THE GRID USED FOR THE EVALUATION
+    // OF THE METRIC
+    std::vector<int> gridStep(3,0);
+    gridStep[0] = 64;
+    gridStep[1] = 64;
+    gridStep[2] = 64;
+   
+    
+    std::cout << "Reading images " << std::endl ;
+    
+    ImageT::Pointer fixedImage = ImageT::New();
+       
+    ImageFileReaderT::Pointer 
+    imageFileReaderFixed = ImageFileReaderT::New();
+    imageFileReaderFixed->SetFileName(filenameFixed);
+    fixedImage = imageFileReaderFixed->GetOutput();
+    fixedImage->Update();
+       
+    ImageT::Pointer movingImage = ImageT::New();
+    
+    
+    ImageFileReaderT::Pointer 
+           imageFileReaderMoving = ImageFileReaderT::New();
+    imageFileReaderMoving->SetFileName(filenameMoving);
+    movingImage = imageFileReaderMoving->GetOutput();
+    movingImage->Update();
+
+    ImageT::SizeType  imageSize;
+    imageSize = fixedImage->GetLargestPossibleRegion().GetSize();
+    
+    
+    // Here we define the points where the similarity metric is evaluated. The
+    // patches are extracted setting as center each point of the evaluationGrid
+    // vector
+    
+    std::vector<ImageT::IndexType> evaluationGrid;
+    std::vector<int> upperGridLimits(3,0);
+    std::vector<int> lowerGridLimits(3,0);
+    
+    upperGridLimits[0] = imageSize[0] - patchSize[0];
+    upperGridLimits[1] = imageSize[1] - patchSize[1];
+    upperGridLimits[2] = imageSize[2] - patchSize[1];
+    
+    lowerGridLimits[0] = 0;
+    lowerGridLimits[1] = 0;
+    lowerGridLimits[2] = 0;
+    
+    
+    int x,y,z;
+    
+    z = lowerGridLimits[2];
+    
+    while (z <= upperGridLimits[2])
+    {
+        y = lowerGridLimits[1];
+        while ( y <= upperGridLimits[1])
+        {
+            x = lowerGridLimits[0];
+            while ( x <= upperGridLimits[0])
+            {
+                ImageT::IndexType currentGridPoint;
+                currentGridPoint[0] = x;
+                currentGridPoint[1] = y;
+                currentGridPoint[2] = z;
+                evaluationGrid.push_back(currentGridPoint);
+                x += gridStep[0];
+                
+            }
+            y += gridStep[1];
+        }
+        z += gridStep[2];
+    }
+    
+    
+    NnSimilarityMetric similarityMetric;
+    // Here we setup the similarity metric: 
+    //ONLY INITIALIZE TENSORS AFTER SETTING UP THE GRID AND THE PATCH SIZE. THERE
+    //ARE NO DEFAULTS!!
+
+    similarityMetric.setLuaState();
+    similarityMetric.setPatchSize(patchSize);
+    similarityMetric.setGrid(evaluationGrid);
+    similarityMetric.setFixedImage(fixedImage);
+    similarityMetric.initializeTensors();
+    similarityMetric.setNetwork(netpath);
+    //
+    std::vector<double> optimisationLowerBounds(6);
+    std::vector<double> optimisationUpperBounds(6);
+    std::vector<double> optimisationStepSize(6);
+
+    optimisationLowerBounds[0] = -1;
+    optimisationLowerBounds[1] = -1;
+    optimisationLowerBounds[2] = -1; 
+    optimisationLowerBounds[3] = -30;
+    optimisationLowerBounds[4] = -30;
+    optimisationLowerBounds[5] = -30;
+    
+    optimisationUpperBounds[0] = 1;
+    optimisationUpperBounds[1] = 1;
+    optimisationUpperBounds[2] = 1; 
+    optimisationUpperBounds[3] = 30;
+    optimisationUpperBounds[4] = 30;
+    optimisationUpperBounds[5] = 30;
+    
+    optimisationStepSize[1] = 0.5;
+    optimisationStepSize[0] = 0.5;
+    optimisationStepSize[2] = 0.5; 
+    optimisationStepSize[3] = 15;
+    optimisationStepSize[4] = 15;
+    optimisationStepSize[5] = 15;
+    
+    NloptDataStruct dataStructure;
+    dataStructure.m_fixedImage = fixedImage;
+    dataStructure.m_movingImage = movingImage;
+    dataStructure.m_similarityMetric = &similarityMetric;
+    
+    nlopt::opt optimisation(nlopt::LN_BOBYQA,6);
+    optimisation.set_lower_bounds(optimisationLowerBounds);
+    optimisation.set_upper_bounds(optimisationUpperBounds);
+    optimisation.set_initial_step(optimisationStepSize);
+    optimisation.set_max_objective(nloptSimilarityFunction,&dataStructure);
+    optimisation.set_xtol_rel(0.01);
+    double maximumSimilarity;
+    std::vector<double> transformationVector(6);
+    std::fill(transformationVector.begin(),
+              transformationVector.end(),0);
+    
+    optimisation.optimize(transformationVector,maximumSimilarity);
+    
+    std::cout << "Optimal transformation is: " ;
+    for (int i=0; i<6;i++)
+        std::cout << transformationVector[i] << " ";
+    
+    std::cout << std::endl;
+            
+    
+    
+    
+}
+
+
